2db99ef8995332e56e3d0676ab30675d39bbee72
HoangNghia <hoangnghianguyendac@gmail.com>
2023-05-24 21:54:56 +0700

    section list

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,36 +1,47 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet, FlatList } from "react-native";
+import { View, Text, ScrollView, StyleSheet, SectionList } from "react-native";
 
 const green = "#495E57";
 const yellow = "#F4CE14";
 
 const menuItemsToDisplay = [
-    { name: "Hummus", id: "1A" },
-    { name: "Moutabal", id: "2B" },
-    { name: "Falafel", id: "3C" },
-    { name: "Marinated Olives", id: "4D" },
-    { name: "Kofta", id: "5E" },
-    { name: "Eggplant Salad", id: "6F" },
-    { name: "Lentil Burger", id: "7G" },
-    { name: "Smoked Salmon", id: "8H" },
-    { name: "Kofta Burger", id: "9I" },
-    { name: "Turkish Kebab", id: "10J" },
-    { name: "Fries", id: "11K" },
-    { name: "Buttered Rice", id: "12L" },
-    { name: "Bread Sticks", id: "13M" },
-    { name: "Pita Pocket", id: "14N" },
-    { name: "Lentil Soup", id: "15O" },
-    { name: "Greek Salad", id: "16Q" },
-    { name: "Rice Pilaf", id: "17R" },
-    { name: "Baklava", id: "18S" },
-    { name: "Tartufo", id: "19T" },
-    { name: "Tartufo", id: "20U" },
-    { name: "Tiramisu", id: "21V" },
-    { name: "Panna Cotta", id: "22W" },
+    {
+        title: 'Appetizers',
+        data: [
+            'Hummus',
+            'Moutabal',
+            'Falafel',
+            'Marinated Olives',
+            'Kofta',
+            'Eggplant Salad',
+        ],
+    },
+    {
+        title: 'Main Dishes',
+        data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+    },
+    {
+        title: 'Sides',
+        data: [
+            'Fries',
+            'Buttered Rice',
+            'Bread Sticks',
+            'Pita Pocket',
+            'Lentil Soup',
+            'Greek Salad',
+            'Rice Pilaf',
+        ],
+    },
+    {
+        title: 'Desserts',
+        data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+    },
 ];
+
 const header = () => <Text style={menuStyle.headerText}>View Menu</Text>
 const footer = () => <Text style={menuStyle.footerText}>All right reserved by Little Lemon 2022</Text>
 const separator = () => <View style={menuStyle.separator}> </View>;
+
 const Item = ({ name }) => {
     return (
         <View style={menuStyle.container}>
@@ -41,19 +52,16 @@ const Item = ({ name }) => {
 
 const MenuItem = () => {
     const renderItem = ({ item }) => {
-        return <Item name={item.name}></Item>;
+        console.log('item>>', item);
+        return <Item name={item}></Item>;
     };
+
+    const renderSectionHeader = ({ section: { title } }) => <Text style={menuStyle.headerText}>{title}</Text>
 
     return (
         <View style={menuStyle.container}>
-            <FlatList
-                data={menuItemsToDisplay}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id}
-                ItemSeparatorComponent={separator}
-                ListHeaderComponent={header}
-                ListFooterComponent={footer}
-            />
+            <SectionList sections={menuItemsToDisplay} renderItem={renderItem}
+                renderSectionHeader={renderSectionHeader} ItemSeparatorComponent={separator} ListFooterComponent={footer} />
         </View>
     );
 };
@@ -61,17 +69,19 @@ const MenuItem = () => {
 export default MenuItem;
 const menuStyle = StyleSheet.create({
     container: {
-        flex: 0.75,
+        flex: 0.95,
     },
     innerContainer: {
-        padding: 40,
+        paddingHorizontal: 40,
+        paddingVertical: 20,
         backgroundColor: green,
     },
     headerText: {
-        fontSize: 40,
+        fontSize: 30,
         flexWrap: "wrap",
-        color: "white",
+        color: "black",
         textAlign: "center",
+        backgroundColor: "#F6D5B8"
     },
     innerText: {
         color: yellow,
