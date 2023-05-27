444a1944a1434fabeb05bb64ea9cc764e5619301
HoangNghia <hoangnghianguyendac@gmail.com>
2023-05-27 08:01:30 +0700

    passing prop to text input

--- components/LittleLemonFooter.js
+++ components/LittleLemonFooter.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from "react";
+import { View, Text } from "react-native";
 
 export default function LittleLemonHeader() {
     return (
         <View style={{ flex: 1, backgroundColor: "#F4CE14" }}>
-            <Text style={{ padding: 30, fontSize: 30, color: "black" }}> Little Lemon</Text>
+            <Text style={{ padding: 30, fontSize: 30, color: "black" }}>
+                {" "}
+                Little Lemon
+            </Text>
         </View>
-    )
+    );
 }
--- components/TextInputComponent.js
+++ components/TextInputComponent.js
@@ -1,36 +1,70 @@
-import React, { useState } from 'react'
-import { StyleSheet, View, Text, TextInput, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    ScrollView,
+    KeyboardAvoidingView,
+    Platform,
+} from "react-native";
 
 export default function FeedBackForm() {
-    const [firstName, onchangeFirstName] = useState("")
-    const [lastName, onchangeLastName] = useState("")
-    const [message, onchangeMessage] = useState("")
+    const [firstName, onchangeFirstName] = useState("");
+    const [lastName, onchangeLastName] = useState("");
+    const [message, onchangeMessage] = useState("");
+    const [phoneNum, onchangePhoneNum] = useState("");
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <ScrollView keyboardDismissMode='on-drag'>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <ScrollView keyboardDismissMode="on-drag">
                 <Text style={styles.headingSection}>
                     How was you visit to little lemon?
                 </Text>
                 <Text style={styles.infoSection}>
-                    Little Lemon is a charming neighborhood bistro that servers simple food and
-                    classic cocktails in a lively but casual environment. We would love to hear
-                    your experience with us
+                    Little Lemon is a charming neighborhood bistro that servers
+                    simple food and classic cocktails in a lively but casual
+                    environment. We would love to hear your experience with us
                 </Text>
 
-                <TextInput style={styles.input} value={firstName} onChangeText={onchangeFirstName} placeholder='First name' />
-                <TextInput style={styles.input} value={lastName} onChangeText={onchangeLastName} placeholder='Last name' />
-                <TextInput style={styles.messageInput} value={message} onChangeText={onchangeMessage} placeholder='Message' />
+                <TextInput
+                    style={styles.input}
+                    value={firstName}
+                    onChangeText={onchangeFirstName}
+                    placeholder="First name"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={lastName}
+                    onChangeText={onchangeLastName}
+                    placeholder="Last name"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={phoneNum}
+                    onChangeText={onchangePhoneNum}
+                    placeholder="Phone Number"
+                />
+                <TextInput
+                    style={styles.messageInput}
+                    value={message}
+                    multiline={true}
+                    onChangeText={onchangeMessage}
+                    placeholder="Please leave feedback"
+                    keyboardType="phone-pad"
+                />
             </ScrollView>
         </KeyboardAvoidingView>
-
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'rgb(63, 84, 79)',
+        backgroundColor: "rgb(63, 84, 79)",
     },
     input: {
         height: 40,
@@ -38,8 +72,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         fontSize: 16,
-        borderColor: 'EDEFEE',
-        backgroundColor: '#F4CE14',
+        borderColor: "EDEFEE",
+        backgroundColor: "#F4CE14",
     },
     messageInput: {
         height: 100,
@@ -47,21 +81,21 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         fontSize: 16,
-        backgroundColor: '#F4CE14'
+        backgroundColor: "#F4CE14",
     },
     infoSection: {
         fontSize: 20,
         padding: 20,
         marginVertical: 8,
-        color: '#EDEFEE',
-        textAlign: 'center'
+        color: "#EDEFEE",
+        textAlign: "center",
     },
     headingSection: {
         fontSize: 28,
         padding: 30,
         marginVertical: 8,
-        color: '#EDEFEE',
-        textAlign: 'center',
-        backgroundColor: '#3f544f'
-    }
-})
+        color: "#EDEFEE",
+        textAlign: "center",
+        backgroundColor: "#3f544f",
+    },
+});
