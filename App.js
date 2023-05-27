4ac43ff2d401fb475d31e1d02a81dec912802c58
HoangNghia <hoangnghianguyendac@gmail.com>
2023-05-27 09:46:56 +0700

    pressable lelement

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextComponent, View } from 'react-native';
-import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonHeader';
-import MenuItem from './components/MenuItems';
-import FeedBackForm from './components/TextInputComponent';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextComponent, View } from "react-native";
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonHeader";
+import MenuItem from "./components/MenuItems";
+import FeedBackForm from "./components/TextInputComponent";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <LittleLemonHeader />
-      <MenuItem /> */}
-      <FeedBackForm />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <LittleLemonHeader />
+            <MenuItem />
+            {/* <FeedBackForm /> */}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#495E57'
-  }
-})
+    container: {
+        flex: 1,
+        backgroundColor: "#495E57",
+    },
+});
--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,45 +1,61 @@
-import React from "react";
-import { View, Text, ScrollView, StyleSheet, SectionList } from "react-native";
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    ScrollView,
+    StyleSheet,
+    SectionList,
+    Pressable,
+} from "react-native";
 
 const green = "#495E57";
 const yellow = "#F4CE14";
 
 const menuItemsToDisplay = [
     {
-        title: 'Appetizers',
+        title: "Appetizers",
         data: [
-            'Hummus',
-            'Moutabal',
-            'Falafel',
-            'Marinated Olives',
-            'Kofta',
-            'Eggplant Salad',
+            "Hummus",
+            "Moutabal",
+            "Falafel",
+            "Marinated Olives",
+            "Kofta",
+            "Eggplant Salad",
         ],
     },
     {
-        title: 'Main Dishes',
-        data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
-    },
-    {
-        title: 'Sides',
+        title: "Main Dishes",
         data: [
-            'Fries',
-            'Buttered Rice',
-            'Bread Sticks',
-            'Pita Pocket',
-            'Lentil Soup',
-            'Greek Salad',
-            'Rice Pilaf',
+            "Lentil Burger",
+            "Smoked Salmon",
+            "Kofta Burger",
+            "Turkish Kebab",
         ],
     },
     {
-        title: 'Desserts',
-        data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+        title: "Sides",
+        data: [
+            "Fries",
+            "Buttered Rice",
+            "Bread Sticks",
+            "Pita Pocket",
+            "Lentil Soup",
+            "Greek Salad",
+            "Rice Pilaf",
+        ],
+    },
+    {
+        title: "Desserts",
+        data: ["Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
     },
 ];
 
-const header = () => <Text style={menuStyle.headerText}>View Menu</Text>
-const footer = () => <Text style={menuStyle.footerText}>All right reserved by Little Lemon 2022</Text>
+const header = () => <Text style={menuStyle.headerText}>View Menu</Text>;
+const footer = () => (
+    <Text style={menuStyle.footerText}>
+        All right reserved by Little Lemon 2022
+    </Text>
+);
 const separator = () => <View style={menuStyle.separator}> </View>;
 
 const Item = ({ name }) => {
@@ -50,23 +66,49 @@ const Item = ({ name }) => {
     );
 };
 
-const MenuItem = () => {
+export default function MenuItem() {
+    const [openMenu, setOpenMenu] = useState(false);
+
     const renderItem = ({ item }) => {
-        console.log('item>>', item);
+        console.log("item>>", item);
         return <Item name={item}></Item>;
     };
 
-    const renderSectionHeader = ({ section: { title } }) => <Text style={menuStyle.headerText}>{title}</Text>
+    const renderSectionHeader = ({ section: { title } }) => (
+        <Text style={menuStyle.headerText}>{title}</Text>
+    );
 
     return (
         <View style={menuStyle.container}>
-            <SectionList sections={menuItemsToDisplay} renderItem={renderItem}
-                renderSectionHeader={renderSectionHeader} ItemSeparatorComponent={separator} ListFooterComponent={footer} />
+            <Text style={menuStyle.infoSection}>
+                Little Lemon is a charming neighborhood bistro that serves
+                simple food and classic cocktails in a lively but casual
+                environment. View our menu to explore our cuisine with daily
+                specials!
+            </Text>
+
+            <Pressable
+                style={menuStyle.button}
+                onPress={() => setOpenMenu(!openMenu)}
+            >
+                <Text style={menuStyle.buttonText}>
+                    {!openMenu ? "View Menu" : "Home"}
+                </Text>
+            </Pressable>
+
+            {openMenu && (
+                <SectionList
+                    sections={menuItemsToDisplay}
+                    renderItem={renderItem}
+                    renderSectionHeader={renderSectionHeader}
+                    ItemSeparatorComponent={separator}
+                    ListFooterComponent={footer}
+                />
+            )}
         </View>
     );
-};
+}
 
-export default MenuItem;
 const menuStyle = StyleSheet.create({
     container: {
         flex: 0.95,
@@ -81,22 +123,45 @@ const menuStyle = StyleSheet.create({
         flexWrap: "wrap",
         color: "black",
         textAlign: "center",
-        backgroundColor: "#F6D5B8"
+        backgroundColor: "#F6D5B8",
     },
     innerText: {
         color: yellow,
         fontSize: 30,
         paddingBottom: 20,
-        paddingLeft: 30
+        paddingLeft: 30,
     },
     separator: {
         borderBottomWidth: 1,
         borderColor: "#EDEFFE",
     },
     footerText: {
-        color: '#000000',
+        color: "#000000",
         padding: 10,
         backgroundColor: yellow,
-        textAlign: 'center'
-    }
+        textAlign: "center",
+    },
+    button: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 40,
+        backgroundColor: "#EDEFEE",
+        borderColor: "#EDEFEE",
+        borderWidth: 2,
+        borderRadius: 12,
+    },
+    buttonText: {
+        color: "#333333",
+        textAlign: "center",
+        fontSize: 32,
+    },
+    infoSection: {
+        fontSize: 24,
+        padding: 20,
+        marginVertical: 8,
+        color: "#EDEFEE",
+        textAlign: "center",
+        backgroundColor: "#495E57",
+    },
 });
