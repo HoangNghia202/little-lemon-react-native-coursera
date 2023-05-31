f5d01dbe308f41628d97e28cc8d16e7aece04f6d
HoangNghia <hoangnghianguyendac@gmail.com>
2023-05-31 09:39:57 +0700

    bottom tab navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TextComponent, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonHeader";
 import MenuItem from "./components/MenuItems";
@@ -9,8 +10,11 @@ import FeedBackForm from "./components/TextInputComponent";
 import Welcome from "./components/welcome";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import MenuScreen from "./screens/MenuScreen";
+import { Ionicons } from "@expo//vector-icons";
+import TabBarIcon from "@react-navigation/bottom-tabs/src/views/TabBarIcon";
 
 const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 function LogoTitle() {
     return (
         <Image
@@ -27,30 +31,57 @@ function LogoTitle() {
 
 export default function App() {
     return (
+        // <NavigationContainer>
+        //     <Stack.Navigator
+        //         initialRouteName="Welcome"
+        //         screenOptions={{
+        //             headerStyle: {
+        //                 backgroundColor: "#333333",
+        //                 textAlign: "center",
+        //             },
+        //             headerTintColor: "#fff",
+        //             headerTitleStyle: {
+        //                 fontWeight: "bold",
+        //             },
+        //         }}
+        //     >
+        //         <Stack.Screen
+        //             name="Welcome"
+        //             component={WelcomeScreen}
+        //             options={{
+        //                 title: "Home",
+        //                 // headerTitle: (props) => <LogoTitle {...props} />,
+        //             }}
+        //         />
+        //         <Stack.Screen name="Menu" component={MenuScreen} />
+        //     </Stack.Navigator>
+        // </NavigationContainer>
+
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="Welcome"
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: "#333333",
-                        textAlign: "center",
-                    },
-                    headerTintColor: "#fff",
-                    headerTitleStyle: {
-                        fontWeight: "bold",
-                    },
+            <BottomTab.Navigator
+                screenOptions={(route) => {
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === "Welcome") {
+                            iconName = focused
+                                ? "ios-information-circle"
+                                : "ios-information-circle-outline";
+                        } else if (route.name === "Menu") {
+                            iconName = focused ? "ios-list-box" : "ios-list";
+                        }
+                        return (
+                            <Ionicons
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    };
                 }}
             >
-                <Stack.Screen
-                    name="Welcome"
-                    component={WelcomeScreen}
-                    options={{
-                        title: "Home",
-                        headerTitle: (props) => <LogoTitle {...props} />,
-                    }}
-                />
-                <Stack.Screen name="Menu" component={MenuScreen} />
-            </Stack.Navigator>
+                <BottomTab.Screen name="Welcome" component={WelcomeScreen} />
+                <BottomTab.Screen name="Menu" component={MenuScreen} />
+            </BottomTab.Navigator>
         </NavigationContainer>
     );
 }
